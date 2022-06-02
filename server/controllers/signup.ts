cd7a3b7ec9ec45960b316f5ff2d7991932199bf4
cptkuk91 <cptkuk91@gmail.com>
2022-06-02 23:44:34 +0900

    [server] 수정: TypeScript 세팅중

--- server/controllers/signup.ts
+++ server/controllers/signup.ts
@@ -3,3 +3,6 @@ import { Request, Response } from "express";
 export async function signup(req: Request, res: Response) {
   return res.end();
 }
+
+// auth/signup으로 나눠야 할지
+// auth 내에 전체적으로 내용을 넣을지 고민해보기
